64c031aa2937e5b8b4952db9a2d8ca7fb426cc5a
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-11-08 20:33:57 +0100

    interactive display hotfix

--- Runtime/Scripts/TeleportationScenePortal.cs
+++ Runtime/Scripts/TeleportationScenePortal.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -10,14 +11,41 @@ public class TeleportationScenePortal : BaseTeleportationInteractable
     public Transform exitDestination;
     protected override bool GenerateTeleportRequest(IXRInteractor interactor, RaycastHit raycastHit, ref TeleportRequest teleportRequest)
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Transform playerRoot = player.transform;
+
+        while (playerRoot.parent != null)
+        {
+            playerRoot = playerRoot.parent;
+        }
         DontDestroyOnLoad(exitDestination.gameObject);
+        DontDestroyOnLoad(playerRoot);
+
         Scene enterScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(exitSceneName, LoadSceneMode.Single);
-              
+
+        //remove other event systems
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject playerInstance in allPlayers)
+        {
+            Debug.Log(playerInstance);
+            Transform playerInstanceRoot = playerInstance.transform;
+
+            while (playerInstanceRoot.parent != null)
+            {
+                playerInstanceRoot = playerInstanceRoot.parent;
+            }
+            if(playerInstanceRoot != playerRoot)
+            {
+                Destroy(playerInstanceRoot.gameObject);
+            }
+        }
+
         teleportRequest.destinationPosition = exitDestination.position;
         teleportRequest.destinationRotation = exitDestination.rotation;
 
-        //Destroy(exitDestination.gameObject);
+        Destroy(exitDestination.gameObject);
 
         return true;
     }
