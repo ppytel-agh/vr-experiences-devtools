41e1f3fdc2194c983bab017affe771a2c8bf0739
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-11-08 20:56:24 +0100

    temporaryly disable persisting player on teleprot between scenes

--- Runtime/Scripts/TeleportationScenePortal.cs
+++ Runtime/Scripts/TeleportationScenePortal.cs
@@ -11,41 +11,41 @@ public class TeleportationScenePortal : BaseTeleportationInteractable
     public Transform exitDestination;
     protected override bool GenerateTeleportRequest(IXRInteractor interactor, RaycastHit raycastHit, ref TeleportRequest teleportRequest)
     {
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Transform playerRoot = player.transform;
+        // GameObject player = GameObject.FindGameObjectWithTag("Player");
+        // Transform playerRoot = player.transform;
 
-        while (playerRoot.parent != null)
-        {
-            playerRoot = playerRoot.parent;
-        }
-        DontDestroyOnLoad(exitDestination.gameObject);
-        DontDestroyOnLoad(playerRoot);
+        // while (playerRoot.parent != null)
+        // {
+            // playerRoot = playerRoot.parent;
+        // }
+        // DontDestroyOnLoad(exitDestination.gameObject);
+        //DontDestroyOnLoad(playerRoot);
 
-        Scene enterScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(exitSceneName, LoadSceneMode.Single);
+        // Scene enterScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(this.exitSceneName, LoadSceneMode.Single);
 
         //remove other event systems
-        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        // GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
 
-        foreach (GameObject playerInstance in allPlayers)
-        {
-            Debug.Log(playerInstance);
-            Transform playerInstanceRoot = playerInstance.transform;
+        // foreach (GameObject playerInstance in allPlayers)
+        // {
+            // Debug.Log(playerInstance);
+            // Transform playerInstanceRoot = playerInstance.transform;
 
-            while (playerInstanceRoot.parent != null)
-            {
-                playerInstanceRoot = playerInstanceRoot.parent;
-            }
-            if(playerInstanceRoot != playerRoot)
-            {
-                Destroy(playerInstanceRoot.gameObject);
-            }
-        }
+            // while (playerInstanceRoot.parent != null)
+            // {
+                // playerInstanceRoot = playerInstanceRoot.parent;
+            // }
+            // if(playerInstanceRoot != playerRoot)
+            // {
+                // Destroy(playerInstanceRoot.gameObject);
+            // }
+        // }
 
-        teleportRequest.destinationPosition = exitDestination.position;
-        teleportRequest.destinationRotation = exitDestination.rotation;
+        // teleportRequest.destinationPosition = exitDestination.position;
+        // teleportRequest.destinationRotation = exitDestination.rotation;
 
-        Destroy(exitDestination.gameObject);
+        // Destroy(exitDestination.gameObject);
 
         return true;
     }
