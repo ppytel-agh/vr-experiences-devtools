07b3d522c9dde3c0852c3ac9db3ae62d215ca7a0
Paweł Pytel <ppytel@student.agh.edu.pl>
2023-12-16 16:58:10 +0100

    implemented scene teleportation where previous rig replaces new scene rig

--- Runtime/Scripts/TeleportationScenePortal.cs
+++ Runtime/Scripts/TeleportationScenePortal.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.LowLevel;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
 
@@ -9,44 +10,88 @@ public class TeleportationScenePortal : BaseTeleportationInteractable
 {
     public string exitSceneName;
     public Transform exitDestination;
+    private Transform playerRoot;
     protected override bool GenerateTeleportRequest(IXRInteractor interactor, RaycastHit raycastHit, ref TeleportRequest teleportRequest)
     {
-        // GameObject player = GameObject.FindGameObjectWithTag("Player");
-        // Transform playerRoot = player.transform;
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        this.playerRoot = player.transform;
 
-        // while (playerRoot.parent != null)
-        // {
-            // playerRoot = playerRoot.parent;
-        // }
+        while (playerRoot.parent != null)
+        {
+            this.playerRoot = playerRoot.parent;
+        }
         DontDestroyOnLoad(exitDestination.gameObject);
-        //DontDestroyOnLoad(playerRoot);
+        DontDestroyOnLoad(this.playerRoot);
 
         // Scene enterScene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene(this.exitSceneName, LoadSceneMode.Single);
+        this.transfer();
+        //SceneManager.LoadScene(this.exitSceneName, LoadSceneMode.Single);
 
-        //remove other event systems
-        // GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
 
-        // foreach (GameObject playerInstance in allPlayers)
-        // {
-            // Debug.Log(playerInstance);
-            // Transform playerInstanceRoot = playerInstance.transform;
-
-            // while (playerInstanceRoot.parent != null)
-            // {
-                // playerInstanceRoot = playerInstanceRoot.parent;
-            // }
-            // if(playerInstanceRoot != playerRoot)
-            // {
-                // Destroy(playerInstanceRoot.gameObject);
-            // }
-        // }
 
         teleportRequest.destinationPosition = exitDestination.position;
         teleportRequest.destinationRotation = exitDestination.rotation;
 
-        Destroy(exitDestination.gameObject);
+       //Destroy(exitDestination.gameObject);
 
         return true;
+    }
+
+    private void transfer()
+    {
+        string previousSceneName = SceneManager.GetActiveScene().name;
+        DontDestroyOnLoad(this.gameObject);
+        StartCoroutine(sceneLoadCoroutine(previousSceneName, this.exitSceneName));
+    }
+
+    IEnumerator sceneLoadCoroutine(string previousSceneName, string destinationSceneName)
+    {
+        Debug.Log("entered scene load coroutine");
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(destinationSceneName);
+
+        asyncLoad.completed += OnSceneLoadCompleted;
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log("scene load async loop");
+            yield return null;
+        }
+    }
+
+    private void OnSceneLoadCompleted(AsyncOperation asyncOperation)
+    {
+        Debug.Log("post scene load");
+
+        //remove other event systems
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (GameObject playerInstance in allPlayers)
+        {
+            Debug.Log(playerInstance);
+            Transform playerInstanceRoot = playerInstance.transform;
+
+            while (playerInstanceRoot.parent != null)
+            {
+                playerInstanceRoot = playerInstanceRoot.parent;
+            }
+            if (playerInstanceRoot != this.playerRoot)
+            {
+                Destroy(playerInstanceRoot.gameObject);
+            }
+        }
+        //GameObject[] portals = GameObject.FindGameObjectsWithTag("Portal");
+        //foreach (GameObject portal in portals)
+        //{
+        //    string portalDestionation = portal.GetComponent<ScenesPortal>().destinationSceneName;
+        //    if (portalDestionation == previousSceneName)
+        //    {
+        //        player.transform.position = portal.transform.position;
+        //        player.transform.rotation = portal.transform.rotation;
+        //        player.transform.Translate(Vector3.forward * 2.5f);
+        //        break;
+        //    }
+        //}
+        Debug.Log("end of scene load");
     }
 }
